da52ab1f17e07e6487169e00d97418aaa85ecb56
Murilo Erhardt <murilo.erhardt@pucpr.edu.br>
2017-04-07 19:16:33 -0300

    init

--- Aluno/src/aluno/Aluno.java
+++ Aluno/src/aluno/Aluno.java
@@ -1,5 +1,8 @@
-
 package aluno;
+
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
 
 /**
  *
@@ -8,7 +11,22 @@ package aluno;
 public class Aluno {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        while (true) {
+            try {
+                MulticastSocket mcs = new MulticastSocket(12347);
+                InetAddress grp = InetAddress.getByName("239.0.0.1");
+                mcs.joinGroup(grp);
+                byte rec[] = new byte[256];
+                DatagramPacket pkg = new DatagramPacket(rec, rec.length);
+                mcs.receive(pkg);
+                String data = new String(pkg.getData());
+                System.out.println("Dados recebidos:" + data);
+                
+                
+            } catch (Exception e) {
+                System.out.println("Erro: " + e.getMessage());
+            }
+        }
     }
-    
+
 }
--- Coordenador/src/coordenador/Coordenador.java
+++ Coordenador/src/coordenador/Coordenador.java
@@ -1,5 +1,9 @@
 package coordenador;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 /**
  *
  * @author murilo.erhardt
@@ -8,6 +12,18 @@ public class Coordenador {
 
     public static void main(String[] args) {
 
+        try {
+            byte[] b = "teste".getBytes();
+            InetAddress addr = InetAddress.getByName("239.0.0.1");
+            DatagramSocket ds = new DatagramSocket();
+            DatagramPacket pkg = new DatagramPacket(b, b.length, addr, 12347);
+            ds.send(pkg);
+        } catch (Exception e) {
+            System.out.println("Nao foi possivel enviar a mensagem");
+        }
+        while (true) {
+            
+        }
     }
-    
+
 }
